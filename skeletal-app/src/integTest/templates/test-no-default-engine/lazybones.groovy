5c0005e4b91f39166d0335233aeb0349703192f1
Carl Marcum <carl.marcum@codebuilders.net>
2025-11-16 16:24:40 -0500

    chore: replace old groovy-handlebars-engine that is unavailable on bintray with our new one in maven central.

--- skeletal-app/src/integTest/templates/test-no-default-engine/lazybones.groovy
+++ skeletal-app/src/integTest/templates/test-no-default-engine/lazybones.groovy
@@ -1,4 +1,4 @@
-@Grab(group="uk.co.cacoethes", module="groovy-handlebars-engine", version="0.2")
+@Grab(group="net.codebuilders", module="groovy-handlebars-engine", version="0.3.0-M1")
 import uk.co.cacoethes.handlebars.HandlebarsTemplateEngine
 
 registerEngine "hbs", new HandlebarsTemplateEngine()
